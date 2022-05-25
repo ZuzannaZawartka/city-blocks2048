2fd1d65fb0b8753e0d9281d8a67becf8e89f99ee
Zuzia <raf.zuzia7@gmail.com>
2022-05-26 00:20:40 +0200

    feat: server works

--- server/index.js
+++ server/index.js
@@ -1,12 +1,33 @@
 const http = require('http');
-const imageRouter = require("./app/imageRouter")
-const usersRouter = require("./app/usersRouter")
+const recognizeRouter = require("./src/recogniseRequestRouter")
+// const usersRouter = require("./app/usersRouter")
+const fs = require('fs');
 const PORT = 3000;
 
 
 http.createServer(async (req, res) => {
 
     //images
+
+    switch (req.method) {
+        case "GET":
+            //wyświetlenie strony html
+            recognizeRouter(req, res)
+
+            break;
+
+        case "POST":
+            // odbiór posta
+            serverResponse(req, res)
+            break;
+    }
+})
+    .listen(PORT, () => console.log(`listen on ${PORT}`))
+
+
+
+async function serverResponse(req, res) {
+
 
     if (req.url.search("/api/photos") != -1) {
         await imageRouter(req, res)
@@ -23,6 +44,4 @@ http.createServer(async (req, res) => {
     else if (req.url.search("/api/user") != -1) {
         await usersRouter(req, res)
     }
-
-})
-    .listen(PORT, () => console.log(`listen on ${PORT}`))
+}
