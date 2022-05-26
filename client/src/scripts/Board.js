cf65419a0c526109a3c29124cf914cc32d79b7cb
Michal-Piotrkowski <michal.piotrkowski@protonmail.com>
2022-05-26 22:19:29 +0200

    fix: changed params for positioning fields

--- client/src/scripts/Board.js
+++ client/src/scripts/Board.js
@@ -10,18 +10,24 @@ class Board {
 
     generateBoard() {
         let board = new THREE.Object3D();
-        let row = -(this.elementWidth * 3);
-        let column = -(this.elementHeigth * 3);
-        for (let r = 0; r < 5; r++) { // tu masz rowsy
-            for (let c = 0; c < 5; c++) { // tu są columny
+        let row = -(this.elementWidth * 2.5);
+        let column = -(this.elementHeigth * 2.5);
+        for (let r = 0; r < 6; r++) { // tu masz rowsy
+            this.fields[r] = []
+            for (let c = 0; c < 6; c++) { // tu są columny
                 let boardField = new Field(this.elementWidth, this.elementHeigth, this.elementDepth, row, column) // TWORZYMY JEDNO POLE
                 let field = boardField.generate() // GENERUJEMY JE
-                board.add(field) // DODAJEMY
+                console.log(field)
                 this.fields[r][c] = field // PRZEKAZUJEMY DO TABILCY DWUWYMIAROWEJ NASZE POLE (DWU  WYMIAROWA, BO BĘDZIE ŁATWIEJ ODNALEŹĆ)
+                board.add(field) // DODAJEMY
                 row += 30 // ZWIĘKSZAMY WARTOŚCI DO POZYCJI O 30, BO MASZ 6 PÓL OD -90 DO 90 
-                column += 30
             }
+            column += 30
+            row = -75
         }
+        board.position.set(0, 0, 0)
         this.scene.add(board) //DODAJEMY DO SCENY
     }
 }
+
+export default Board
